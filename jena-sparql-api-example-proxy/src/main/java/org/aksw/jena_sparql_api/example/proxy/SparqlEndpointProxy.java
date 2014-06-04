0a2d207d135327005f8a3e80d4c59db541ab73e0
Jorge Cruz <jorge@tvplus.com>
2014-06-04 11:53:17 -0700

    setup proxy caching

--- jena-sparql-api-example-proxy/src/main/java/org/aksw/jena_sparql_api/example/proxy/SparqlEndpointProxy.java
+++ jena-sparql-api-example-proxy/src/main/java/org/aksw/jena_sparql_api/example/proxy/SparqlEndpointProxy.java
@@ -1,5 +1,6 @@
 package org.aksw.jena_sparql_api.example.proxy;
 
+import java.sql.SQLException;
 import java.util.Collection;
 
 import javax.servlet.ServletContext;
@@ -7,6 +8,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 
+import org.aksw.jena_sparql_api.cache.core.QueryExecutionFactoryCacheEx;
+import org.aksw.jena_sparql_api.cache.extra.CacheBackend;
+import org.aksw.jena_sparql_api.cache.extra.CacheFrontend;
+import org.aksw.jena_sparql_api.cache.extra.CacheFrontendImpl;
+import org.aksw.jena_sparql_api.cache.h2.CacheCoreH2;
 import org.aksw.jena_sparql_api.core.QueryExecutionFactory;
 import org.aksw.jena_sparql_api.http.QueryExecutionFactoryHttp;
 import org.aksw.jena_sparql_api.utils.UriUtils;
@@ -65,5 +71,32 @@ public class SparqlEndpointProxy
 		
 		return result;
 	}
+
+	@Override
+	public QueryExecution createQueryExecution(Query query) {
+
+        //create a query execution over dbpedia
+		String serviceUri = "http://dbpedia.org/sparql";
+		QueryExecutionFactory qef = new QueryExecutionFactoryHttp(serviceUri);
+
+        //setup caching
+        try {
+            //cache for a day
+            //name the db 'sparql' and use compression
+            long ttl = 24l * 60l * 60l * 1000l;
+            CacheBackend cacheBackend = CacheCoreH2.create("sparql", ttl, true);
+            CacheFrontend cacheFrontend = new CacheFrontendImpl(cacheBackend);
+
+            qef = new QueryExecutionFactoryCacheEx(qef, cacheFrontend);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        QueryExecution result = qef.createQueryExecution(query);
+
+		return result;
+    }
 }
 
